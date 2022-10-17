2822dcc8f750ff7fafc631d9f0f7f7f2c31ae4fd
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-17 14:37:24 +0200

    Routing

--- video-talker-frontend/src/App.js
+++ video-talker-frontend/src/App.js
@@ -1,13 +1,29 @@
 import { useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { connectWithWebSocket } from "./utils/wsConnection/wsConnection";
+import Dashboard from "./Dashboard/Dashboard";
+import Login from "./Login/Login";
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
   }, []);
 
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
