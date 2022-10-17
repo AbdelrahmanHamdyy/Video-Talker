5fe8c40d32099dfaa69d2b45e2fd30a7ec210e3d
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-17 19:31:33 +0200

    Active Users List

---
+++ video-talker-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ActiveUsersList.css";
+
+const activeUsers = [
+  {
+    socketId: 321,
+    username: "Paul",
+  },
+  {
+    socketId: 333,
+    username: "John",
+  },
+  {
+    socketId: 432,
+    username: "Kate",
+  },
+  {
+    socketId: 345,
+    username: "Adam",
+  },
+];
+
+const ActiveUsersList = () => {
+  return <div>ActiveUsersList</div>;
+};
+
+export default ActiveUsersList;
