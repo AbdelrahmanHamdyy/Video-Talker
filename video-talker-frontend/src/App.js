2ffaeb7e354dafe1c4fcec7f3975fdaf7e600883
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-17 18:59:05 +0200

    Update App.js

--- video-talker-frontend/src/App.js
+++ video-talker-frontend/src/App.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
 import { connectWithWebSocket } from "./utils/wsConnection/wsConnection";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
@@ -11,18 +11,16 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
