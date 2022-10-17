1a56cc4f2b7c88ba00e6ebc3dd4748af81cfba00
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-17 14:23:35 +0200

    Initial State

--- video-talker-frontend/src/App.js
+++ video-talker-frontend/src/App.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
+import { connectWithWebSocket } from "./utils/wsConnection/wsConnection";
 
 function App() {
+  useEffect(() => {
+    connectWithWebSocket();
+  }, []);
+
   return <div className="App">React App</div>;
 }
 
