7b77440d0920407c2ea910519074cf3cb8546bbb
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-18 22:19:42 +0200

    Getting active users as props

--- video-talker-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
+++ video-talker-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
@@ -1,27 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
 import ActiveUsersListItem from "./ActiveUsersListItem";
 import "./ActiveUsersList.css";
 
-const activeUsers = [
-  {
-    socketId: 321,
-    username: "Paul",
-  },
-  {
-    socketId: 333,
-    username: "John",
-  },
-  {
-    socketId: 432,
-    username: "Kate",
-  },
-  {
-    socketId: 345,
-    username: "Adam",
-  },
-];
-
-const ActiveUsersList = () => {
+const ActiveUsersList = ({ activeUsers }) => {
   return (
     <div className="active_users_list_container">
       {activeUsers.map((activeUser) => (
@@ -34,4 +16,8 @@ const ActiveUsersList = () => {
   );
 };
 
-export default ActiveUsersList;
+const mapStateToProps = ({ dashboard }) => ({
+  ...dashboard,
+});
+
+export default connect(mapStateToProps)(ActiveUsersList);
