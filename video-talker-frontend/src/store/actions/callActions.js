d318678c836b242b0f3f0021971d939f0078ff92
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-20 14:28:46 +0200

    Calling dialog and username actions

--- video-talker-frontend/src/store/actions/callActions.js
+++ video-talker-frontend/src/store/actions/callActions.js
@@ -1,5 +1,8 @@
 export const CALL_SET_LOCAL_STREAM = "CALL_SET_LOCAL_STREAM";
 export const CALL_SET_CALL_STATE = "CALL_SET_CALL_STATE";
+export const CALL_SET_CALLING_DIALOG_VISIBLE =
+  "CALL_SET_CALLING_DIALOG_VISIBLE";
+export const CALL_SET_CALLER_USERNAME = "CALL_SET_CALLER_USERNAME";
 
 export const callStates = {
   CALL_UNAVAILABLE: "CALL_UNAVAILABLE",
@@ -19,5 +22,19 @@ export const setCallState = (callState) => {
   return {
     type: CALL_SET_CALL_STATE,
     callState,
+  };
+};
+
+export const setCallingDialogVisible = (visible) => {
+  return {
+    type: CALL_SET_CALLING_DIALOG_VISIBLE,
+    visible,
+  };
+};
+
+export const setCallerUsername = (callerUsername) => {
+  return {
+    type: CALL_SET_CALLER_USERNAME,
+    callerUsername,
   };
 };
