0f697ab16f46a375a1ebee3a84832d02684277ec
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-20 14:28:01 +0200

    Control the call dialogs
    Set a condition for each one to appear whenever the state is matched

--- video-talker-frontend/src/Dashboard/components/DirectCall/DirectCall.js
+++ video-talker-frontend/src/Dashboard/components/DirectCall/DirectCall.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { callStates } from "../../../store/actions/callActions";
 import CallingDialog from "../CallingDialog/CallingDialog";
 import CallRejectedDialog from "../CallRejectedDialog/CallRejectedDialog";
 import IncomingCallDialog from "../IncomingCallDialog/IncomingCallDialog";
@@ -7,15 +8,23 @@ import LocalVideoView from "../LocalVideoView/LocalVideoView";
 import RemoteVideoView from "../RemoteVideoView/RemoteVideoView";
 
 const DirectCall = (props) => {
-  const { localStream, remoteStream } = props;
+  const {
+    localStream,
+    remoteStream,
+    callState,
+    callerUsername,
+    callingDialogVisible,
+  } = props;
 
   return (
     <div>
       <LocalVideoView localStream={localStream} />
       {remoteStream && <RemoteVideoView remoteStream={remoteStream} />}
-      {/* <CallRejectedDialog />
-      <IncomingCallDialog />
-      <CallingDialog /> */}
+      <CallRejectedDialog />
+      {callState === callStates.CALL_REQUESTED && (
+        <IncomingCallDialog callerUsername={callerUsername} />
+      )}
+      {callingDialogVisible && <CallingDialog />}
     </div>
   );
 };
