b8e3c0606b0d0ab3eeb8fca347d94b19a790eea4
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-21 14:37:15 +0200

    Disallow calling when already in a call

--- video-talker-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
+++ video-talker-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
@@ -3,21 +3,24 @@ import { connect } from "react-redux";
 import ActiveUsersListItem from "./ActiveUsersListItem";
 import "./ActiveUsersList.css";
 
-const ActiveUsersList = ({ activeUsers }) => {
+const ActiveUsersList = (props) => {
+  const { activeUsers, callState } = props;
   return (
     <div className="active_users_list_container">
       {activeUsers.map((activeUser) => (
         <ActiveUsersListItem
           key={activeUser.socketId}
           activeUser={activeUser}
+          callState={callState}
         />
       ))}
     </div>
   );
 };
 
-const mapStateToProps = ({ dashboard }) => ({
+const mapStateToProps = ({ dashboard, call }) => ({
   ...dashboard,
+  ...call,
 });
 
 export default connect(mapStateToProps)(ActiveUsersList);
