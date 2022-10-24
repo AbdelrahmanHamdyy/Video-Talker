eaf408f70c9f87d0f2c210c01856308b9448b733
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-10-24 23:56:16 +0200

    Minor changes

--- video-talker-frontend/src/Dashboard/components/GroupCall/GroupCall.js
+++ video-talker-frontend/src/Dashboard/components/GroupCall/GroupCall.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import GroupCallButton from "../GroupCallButton/GroupCallButton";
-import { callStates } from "../../../store/actions/callActions";
+import {
+  callStates,
+  setLocalCameraEnabled,
+  setLocalMicrophoneEnabled,
+} from "../../../store/actions/callActions";
 import * as webRTCGroupCallHandler from "../../../utils/webRTC/webRTCGroupCallHandler";
 import GroupCallRoom from "../GroupCallRoom/GroupCallRoom";
 
 const GroupCall = (props) => {
-  const { callState, localStream, groupCallActive, groupCallStreams } = props;
+  const { callState, localStream, groupCallActive } = props;
 
   const createRoom = () => {
     // Create room for the group call
@@ -24,7 +28,7 @@ const GroupCall = (props) => {
         callState !== callStates.CALL_IN_PROGRESS && (
           <GroupCallButton onClickHandler={createRoom} label="Create Room" />
         )}
-      {groupCallActive && <GroupCallRoom groupCallStreams={groupCallStreams} />}
+      {groupCallActive && <GroupCallRoom {...props} />}
       {groupCallActive && (
         <GroupCallButton label="Leave Room" onClickHandler={leaveRoom} />
       )}
@@ -36,4 +40,11 @@ const mapStateToProps = ({ call }) => ({
   ...call,
 });
 
-export default connect(mapStateToProps)(GroupCall);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCameraEnabled: (enabled) => dispatch(setLocalCameraEnabled(enabled)),
+    setMicEnabled: (enabled) => dispatch(setLocalMicrophoneEnabled(enabled)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroupCall);
